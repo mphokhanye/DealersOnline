b429f33fa4b11f469b301b40beb81bc009441ae3
mphokhanye <120773856+mphokhanye@users.noreply.github.com>
2026-04-25 10:40:37 +0200

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-
 export default defineConfig({
   plugins: [react()],
+  base: "/DealersOnline/", // 
   test: {
     environment: "jsdom",
     globals: true,
